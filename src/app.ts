746521972752854073e1557721c3d2ed955a10df
MAHossain1 <mahpro110@gmail.com>
2024-07-04 08:50:35 +0600

    Created productInterface, model, successfully create a product done.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { ProductRoutes } from './modules/product/product.routes';
 const app: Application = express();
 
+// parsers
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use('/api/v1/products', ProductRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,65 @@
+import { model, Schema } from 'mongoose';
+import {
+  ProductModel,
+  TInventory,
+  TProduct,
+  TVariant,
+} from './product.interface';
+
+const variantSchema = new Schema<TVariant>({
+  type: {
+    type: String,
+    required: [true, 'Type is required.'],
+  },
+  value: {
+    type: String,
+    required: [true, 'Value is required.'],
+  },
+});
+
+const inventorySchema = new Schema<TInventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export const productSchema = new Schema<TProduct>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Product name is required.'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Product description is required.'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required.'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Category is required.'],
+    },
+    variants: {
+      type: [variantSchema],
+      required: [true, 'Variants is required.'],
+    },
+    inventory: {
+      type: inventorySchema,
+      required: [true, 'Inventory is required.'],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const Product = model<TProduct, ProductModel>('Product', productSchema);
