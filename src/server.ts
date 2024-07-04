669fb339b482afea2f1189840bedceda219f1c18
MAHossain1 <mahpro110@gmail.com>
2024-07-04 07:41:25 +0600

    Project initialized with eslint prettier setup.

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { Server } from 'http';
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`Ecommerce backend running on port: ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection is detected, shutting down ...', err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log('Uncaught exception is detected, shutting down ..');
+  process.exit(1);
+});
